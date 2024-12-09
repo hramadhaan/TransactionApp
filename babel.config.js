7777ba3aa667419378d727fb5532bff72491539f
Hanif Ramadhan <hramadhaan20@gmail.com>
2024-12-09 21:36:30 +0700

    feat: add additional package

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@features': './src/features',
+          '@navigation': './src/navigation',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+  ],
 };
